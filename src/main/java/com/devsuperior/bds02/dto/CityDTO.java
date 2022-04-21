87b4a89924c4505af21b32416719cb0dc980af3d
Roberta de Siqueira <designer.roberta@gmail.com>
2022-04-21 00:25:08 -0300

    Increment Lombok

--- src/main/java/com/devsuperior/bds02/dto/CityDTO.java
+++ src/main/java/com/devsuperior/bds02/dto/CityDTO.java
@@ -3,39 +3,23 @@ package com.devsuperior.bds02.dto;
 import java.io.Serializable;
 
 import com.devsuperior.bds02.entities.City;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class CityDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
 	private String name;
 	
-	public CityDTO() {
-	}
-
-	public CityDTO(Long id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-	
 	public CityDTO(City entity) {
 		id = entity.getId();
 		name = entity.getName();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 }
